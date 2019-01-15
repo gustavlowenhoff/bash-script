fc8bcf6db19c9997410941cc38279ad95f70aa36
gustavlowenhoff <46317605+gustavlowenhoff@users.noreply.github.com>
2019-01-15 11:19:15 +0100

    Delete skript-20190115-1.ps1

--- skript-20190115-1.ps1
+++
@@ -1,7 +0,0 @@
-﻿$cert=New-SelfSignedCertificate -Subject "My Code Signing Certificate” -Type CodeSigningCert -CertStoreLocation cert:\LocalMachine\My
-
-Export-Certificate -Cert $cert -FilePath "C:\temp\Mitt-rootCA.crt"
-
-Import-Certificate -CertStoreLocation 'Cert:\LocalMachine\Root' -FilePath "C:\temp\Mitt-rootCA.crt"
-
-Set-AuthenticodeSignature -FilePath C:\temp\skript-20190114-6.ps1 -Certificate $cert
