c660acd4a34d75e51ea45ddb9b914656c22fc92e
gustavlowenhoff <46317605+gustavlowenhoff@users.noreply.github.com>
2019-01-18 14:50:51 +0200

    Add files via upload

---
+++ skript-20190114-7a.ps1
@@ -0,0 +1,16 @@
+﻿New-Item C:\temp\fel.html -ItemType File
+
+$File1="C:\temp\fel.html"
+
+Get-EventLog -LogName System -EntryType Warning,Error | measure | ConvertTo-HTML | Out-File $File1 -Append
+
+Get-EventLog -LogName System -EntryType Warning,Error -After (Get-Date).AddHours(-24) | ConvertTo-HTML | Out-File $File1 -Append
+
+Get-EventLog -LogName System -EntryType Error,Warning -Newest 100 | Sort-Object -Unique | ConvertTo-HTML | Out-File $File1 -Append
+
+$a=Get-EventLog -LogName System -EntryType Error,Warning -Newest 10 | measure
+$a.Count
+
+if ($a.Count -ge 10){
+    Write-Warning ("More then 10 errors!")
+}
