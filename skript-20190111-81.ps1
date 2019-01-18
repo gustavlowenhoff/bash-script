a464da3ebd869963c0f868e3e0dfbee91c948769
gustavlowenhoff <46317605+gustavlowenhoff@users.noreply.github.com>
2019-01-18 14:16:39 +0200

    Add files via upload

---
+++ skript-20190111-81.ps1
@@ -0,0 +1,21 @@
+﻿Get-Content c:\address.txt
+
+$address=(Get-Content c:\address.txt)
+$address
+
+$workaddress = @"
+Nackademin
+Tomtebodagatan 3A
+"@
+$workaddress
+
+$myname = @"
+Gustav
+Löwenhoff
+"@
+$myname
+
+$myname | gm
+
+($myname).ToLower()
+($myname).ToUpper()
