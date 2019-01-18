e07f8c77dbc5e2b3b57a7dc5994df7d6753d6b97
gustavlowenhoff <46317605+gustavlowenhoff@users.noreply.github.com>
2019-01-18 14:17:13 +0200

    Add files via upload

---
+++ skript-20190111-114.ps1
@@ -0,0 +1,17 @@
+﻿$file = Import-Csv "C:\temp\Members.csv"
+
+$file
+
+$file.Protons
+
+($file.Protons | Measure-Object -Average).Average
+
+for ($a = 1; $a -lt $file.Length; $a++)
+{
+    $col0 = ([string]$file[$a]).Split(',')[0]
+    $col2 = ([string]$file[$a]).Split(',')[2]
+    $col3 = ([string]$file[$a]).Split(',')[3]
+        if ($col2 -eq $col3)
+            {echo $col0}
+}
+
