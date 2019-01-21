cdbd19f381228b85d4e5b0adcdbad3f70b2863f0
gustavlowenhoff <46317605+gustavlowenhoff@users.noreply.github.com>
2019-01-21 14:00:02 +0200

    Add files via upload

---
+++ skript-20190114-9.ps1
@@ -0,0 +1,9 @@
+﻿Get-EventLog -LogName Security -InstanceID 4624 -EntryType SuccessAudit | measure | Out-File -FilePath C:\temp\Logins.txt -Encoding default
+
+Get-EventLog "Security" | Where -FilterScript {$_.EventID -eq 4624 -and $_.ReplacementStrings[8] -eq 2} | Out-File -FilePath C:\temp\Logins.txt -Encoding default -Append
+
+Get-EventLog "Security" | Where -FilterScript {$_.EventID -eq 4624 -and $_.ReplacementStrings[8] -eq 3} | Out-File -FilePath C:\temp\Logins.txt -Encoding default -Append
+
+Get-EventLog "Security" | Where -FilterScript {$_.EventID -eq 4624 -and $_.ReplacementStrings[8] -eq 7} | Out-File -FilePath C:\temp\Logins.txt -Encoding default -Append
+
+Get-EventLog -LogName Security -InstanceID 4625 -EntryType FailureAudit | Out-File -FilePath C:\temp\Logins.txt -Encoding default -Append
